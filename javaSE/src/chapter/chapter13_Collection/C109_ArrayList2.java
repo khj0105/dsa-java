e2cc4eadb003c779f610ba6641f5bea0a442f913
khj0105 <khj0001052@gmail.com>
2026-04-24 23:41:35 +0900

    chapter13: Collection

---
+++ javaSE/src/chapter/chapter13_Collection/C109_ArrayList2.java
@@ -0,0 +1,51 @@
+package chapter.chapter13_Collection;
+
+import java.util.ArrayList;
+
+class Student {
+	String name;;
+	int stdNum;
+	
+	public Student(String name, int stdNum) {
+		this.name = name;
+		this.stdNum = stdNum;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getStdNum() {
+		return stdNum;
+	}
+	
+	@Override
+	public String toString() {
+		return "Student{" +
+				"name='" + name + '\'' +
+				", stdNum=" + stdNum +
+				'}';
+	}
+}
+public class C109_ArrayList2 {
+	public static void main(String[] args) {
+		
+		ArrayList<Student> stList = new ArrayList<>();
+		
+		stList.add(new Student("홍길동", 20260001));
+		stList.add(new Student("이순신", 20260002));
+		stList.add(new Student("고길동", 20260003));
+		stList.add(new Student("강감찬", 20260004));
+		
+		System.out.println("=== for문 ===");
+		for (int i = 0; i < stList.size(); i++) {
+			String result = String.format("이름: %s, 학번: %d", stList.get(i).getName(), stList.get(i).getStdNum());
+			System.out.println(result);
+		}
+		
+		System.out.println("=== foreach문 ===");
+		for (Student s : stList) {
+			System.out.println(s);		// toString() 자동 호출
+		}
+	}
+}
